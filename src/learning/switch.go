91ef942e9ef08385adea4372e2d04ada874a051e
Deepak Marathe <deepak.marathe@go-jek.com>
2016-12-06 22:57:14 +0530

    finding answer to when is saturday

--- src/learning/switch.go
+++ src/learning/switch.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"time"
 )
 
 func main() {
@@ -13,7 +14,20 @@ func main() {
 	case "linux" :
 		fmt.Printf("Linux")
 	default :
-		fmt.Printf("%s.",os)
+		fmt.Printf("%s.", os)
 
+	}
+	fmt.Println()
+	fmt.Println("When's Saturday?")
+	today := time.Now().Weekday();
+	switch time.Saturday {
+	case today + 0 :
+		fmt.Printf("Today.")
+	case today + 1:
+		fmt.Printf("Tomorrow")
+	case today + 2:
+		fmt.Printf("Day after tomorrow")
+	default :
+		fmt.Printf("Too far away")
 	}
 }
